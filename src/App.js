df71eb5cd3a3745353766802e5c62a0f32162141
Kauana Henriques <karoltvbl@gmail.com>
2022-07-28 00:12:11 -0300

    feat: (textfield) implementa componente para campos de texto

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
-import Banner from './components/Banner/Banner';
+import Banner from './components/Banner';
+import TextField from './components/TextField';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Banner />
+        <TextField label="Nome" placeholder="Digite seu nome"/>
+        <TextField label="Cargo" placeholder="Digite seu cargo"/>
+        <TextField label="Imagem" placeholder="Digite a url da imagem"/>
       </div>
     );
   }
