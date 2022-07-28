527872eb0e70daea8c935dd18046c601c2419ff3
Kauana Henriques <karoltvbl@gmail.com>
2022-07-27 23:14:52 -0300

    feat: (banner) implementa componente banner

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Banner.css";
+
+function Banner() {
+  return (
+    <div className="banner">
+      <header className="banner">
+        <img src="../img/kau.jpeg" alt="Banner"/>
+      </header>
+    </div>
+  );
+}
+
+export default Banner;
