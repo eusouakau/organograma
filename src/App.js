e26aabbac370d1e08086822337ad2fd58a7d8fe2
Kauana Henriques <karoltvbl@gmail.com>
2022-08-11 22:56:10 -0300

    refac: refatora app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
 import Banner from './components/Banner';
-import TextField from './components/TextField';
+import Formulario from './components/Form';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Banner />
-        <TextField label="Nome" placeholder="Digite seu nome"/>
-        <TextField label="Cargo" placeholder="Digite seu cargo"/>
-        <TextField label="Imagem" placeholder="Digite a url da imagem"/>
-      </div>
+        <Formulario/>
+    </div>
     );
   }
 }
