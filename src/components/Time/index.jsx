c04b535432497d7356e667223adba29d486d5c38
Kauana Henriques <karoltvbl@gmail.com>
2022-08-25 12:15:06 -0300

    feat: implementa componente de time

---
+++ src/components/Time/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Time.css';
+
+const Time = (props) => {
+    return (
+        <section className="time">
+            <h3>{props.nome}</h3>
+        </section>
+    )
+}
+
+export default Time;
