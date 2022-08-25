84fcb7aa271548579082e5a872da3304a1645bef
Kauana Henriques <karoltvbl@gmail.com>
2022-08-25 12:16:50 -0300

    feat: adiciona novo coponente

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Banner from './components/Banner';
 import Formulario from './components/Form';
+import Time from './components/Time';
 
 
 function App() {
-    return (
-      <div className="App">
-        <Banner />
-        <Formulario/>
-    </div>
-    );
+
+  const [participantes, setParticipantes] = useState([]);
+
+  const aoNovoParticipanteAdicionado = (participante) => {
+    setParticipantes([...participantes, participante]);
   }
+
+  return (
+    <div className="App">
+      <Banner />
+      <Formulario aoParticipanteCadastrado={participante => aoNovoParticipanteAdicionado(participante)}/>
+      <Time nome="Time 1"/>
+      <Time nome="Time 2"/>
+      <Time nome="Time 3"/>
+  </div>
+  );
+}
 
 export default App;
