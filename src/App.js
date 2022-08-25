fb64da32e602ea128ce57d661697bae1513bc247
Kauana Henriques <karoltvbl@gmail.com>
2022-08-25 14:33:17 -0300

    refac: deixa itens dinamicos e centraliza o acesso deles

--- src/App.js
+++ src/App.js
@@ -6,6 +6,44 @@ import Time from './components/Time';
 
 function App() {
 
+  const itens = [
+    {
+      nome: 'Item 1',
+      corPrimaria: '#57c278',
+      corSecundaria: '#d9f7e9'
+    },
+    {
+      nome: 'Item 2',
+      corPrimaria: '#82cffa',
+      corSecundaria: '#e8f8ff'
+    },
+    {
+      nome: 'Item 3',
+      corPrimaria: '#a6d157',
+      corSecundaria: '#f0f8e2'
+    },
+    {
+      nome: 'Item 4',
+      corPrimaria: '#e06b69',
+      corSecundaria: '#fde7e8'
+    },
+    {
+      nome: 'Item 5',
+      corPrimaria: '#db6ebf',
+      corSecundaria: '#fae9f5'
+    },
+    {
+      nome: 'Item 6',
+      corPrimaria: '#ffba05',
+      corSecundaria: '#fff5d9'
+    },
+    {
+      nome: 'Item 7',
+      corPrimaria: '#ff8a29',
+      corSecundaria: '#ffeedf'
+    }
+  ];
+
   const [participantes, setParticipantes] = useState([]);
 
   const aoNovoParticipanteAdicionado = (participante) => {
@@ -15,10 +53,8 @@ function App() {
   return (
     <div className="App">
       <Banner />
-      <Formulario aoParticipanteCadastrado={participante => aoNovoParticipanteAdicionado(participante)}/>
-      <Time nome="Time 1"/>
-      <Time nome="Time 2"/>
-      <Time nome="Time 3"/>
+      <Formulario itens={itens.map(item => item.nome)} aoParticipanteCadastrado={participante => aoNovoParticipanteAdicionado(participante)}/>
+      {itens.map(item => <Time key={item.nome} nome={item.nome} corPrimaria={item.corPrimaria} corSecundaria={item.corSecundaria} />)}
   </div>
   );
 }
