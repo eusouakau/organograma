e5a9936ea77b984fc13e2600ad330f0fc4b5de2e
Kauana Henriques <karoltvbl@gmail.com>
2023-01-31 11:20:05 -0300

    add parenteses na condicional

--- src/components/Time/index.jsx
+++ src/components/Time/index.jsx
@@ -3,8 +3,9 @@ import Colaborador from '../Colaborador';
 import './Time.css';
 
 const Time = (props) => {
+    
     return (
-        props.colaboradores.length > 0 &&
+        (props.colaboradores.length > 0) &&
         <section className="time" style={{backgroundColor: props.corSecundaria}}>
             <h3 style={{ borderColor: props.corPrimaria}}>{props.nome}</h3>
             <div className='colaboradores'>
