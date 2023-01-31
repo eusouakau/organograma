d182f4e6e00585b9c01b9d93d09a0249b253f2aa
Kauana Henriques <karoltvbl@gmail.com>
2023-01-31 09:55:51 -0300

    add colaborador

---
+++ src/components/Colaborador/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Colaborador.css';
+
+const Colaborador = () => {
+    return (
+        <div>
+            <div>
+                <img src='http://github.com/eusouakau.png' alt='Kauana Henriques'/>
+            </div>
+            <div>
+                <h4>Kauana Henriques</h4>
+                <h5>Desenvolvedora</h5>
+            </div>
+        </div>
+    )
+}
+
+export default Colaborador;
