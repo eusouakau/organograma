80bcc020ffeaebfa86d306903858ba6ce55b1f89
Kauana Henriques <karoltvbl@gmail.com>
2023-01-31 10:51:55 -0300

    torna infos de colaboradores dinamicas

--- src/components/Colaborador/index.jsx
+++ src/components/Colaborador/index.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import './Colaborador.css';
 
-const Colaborador = () => {
+const Colaborador = (props) => {
     return (
         <div className='colaborador'>
             <div className='cabecalho'>
-                <img src='http://github.com/eusouakau.png' alt='Kauana Henriques'/>
+                <img src={props.imagem} alt={props.nome}/>
             </div>
             <div className='rodape'>
-                <h4>Kauana Henriques</h4>
-                <h5>Desenvolvedora</h5>
+                <h4>{props.nome}</h4>
+                <h5>{props.cargo}</h5>
             </div>
         </div>
     )
